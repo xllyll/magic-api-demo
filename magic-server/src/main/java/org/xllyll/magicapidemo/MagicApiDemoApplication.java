eed94219d7b460eac27c801e2a04795a3210c12b
xllyll <757271398@qq.com>
2025-03-08 15:12:45 +0800

    2.7.10

--- magic-server/src/main/java/org/xllyll/magicapidemo/MagicApiDemoApplication.java
+++ magic-server/src/main/java/org/xllyll/magicapidemo/MagicApiDemoApplication.java
@@ -2,9 +2,19 @@ package org.xllyll.magicapidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 
 @SpringBootApplication
-public class MagicApiDemoApplication {
+public class MagicApiDemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MagicApiDemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MagicApiDemoApplication.class, args);
